1b07ed8de838179da58604cc6668250ba9e98a00
oleg.estekhin <oleg.estekhin@bba08632-a790-11de-ac05-951101a8bf3e>
2009-09-25 10:39:01 +0000

    [maven-scm] copy for tag maven-svn-wagon-1.0

---
+++ tags/maven-svn-wagon-1.0/src/test/java/oe/maven/wagon/providers/svn/HttpsSVNWagonTest.java
@@ -0,0 +1,17 @@
+/*-
+ * Copyright (c) 2009, Oleg Estekhin
+ * All rights reserved.
+ */
+
+package oe.maven.wagon.providers.svn;
+
+import java.io.IOException;
+
+public class HttpsSVNWagonTest extends AbstractSVNWagonTest {
+
+    @Override
+    protected String getTestRepositoryUrl() throws IOException {
+        return "svn:" + "https://localhost/svn/maven-svn-wagon-test-repository";
+    }
+
+}
