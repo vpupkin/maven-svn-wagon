924aa59b0985d595690acacdf299d17c9ee2ec7f
oleg.estekhin <oleg.estekhin@bba08632-a790-11de-ac05-951101a8bf3e>
2009-09-25 06:40:28 +0000

    pom configuration for environment-dependent tests

---
+++ src/test/java/oe/maven/wagon/providers/svn/HttpSVNWagonTest.java
@@ -0,0 +1,17 @@
+/*-
+ * Copyright (c) 2009, Oleg Estekhin
+ * All rights reserved.
+ */
+
+package oe.maven.wagon.providers.svn;
+
+import java.io.IOException;
+
+public class HttpSVNWagonTest extends AbstractSVNWagonTest {
+
+    @Override
+    protected String getTestRepositoryUrl() throws IOException {
+        return "svn:" + "http://localhost/svn/maven-svn-wagon-test-repository";
+    }
+
+}
